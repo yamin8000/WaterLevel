a7628a62a8134f56dd8227fd691768ea28dd07d7
YaMiN <yamin8000@yahoo.com>
2021-09-07 13:25:38 +0430

    added ota feature for programming nodemcu

--- app/src/main/java/com/github/yamin8000/waterlevel/MainActivity.kt
+++ app/src/main/java/com/github/yamin8000/waterlevel/MainActivity.kt
@@ -12,29 +12,29 @@ import okhttp3.*
 import okio.IOException
 
 class MainActivity : AppCompatActivity() {
-    
+
     private val logTag = "<--> "
     private val url = "http://192.168.1.100"
     private val mainScope = CoroutineScope(Dispatchers.Main)
-    
-    private val binding : ActivityMainBinding by lazy(LazyThreadSafetyMode.NONE) {
+
+    private val binding: ActivityMainBinding by lazy(LazyThreadSafetyMode.NONE) {
         ActivityMainBinding.inflate(layoutInflater)
     }
-    
-    override fun onCreate(savedInstanceState : Bundle?) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        
+
         try {
             prepareLogger()
             updateData()
-        } catch (exception : Exception) {
+        } catch (exception: Exception) {
             baseContext?.let {
                 Toast.makeText(it, "erro", Toast.LENGTH_SHORT).show()
             }
         }
     }
-    
+
     private fun updateData() {
         mainScope.launch {
             while (true) {
@@ -48,14 +48,14 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-    
+
     private fun showNoDataError() {
         baseContext?.let {
             Toast.makeText(baseContext, "error", Toast.LENGTH_SHORT).show()
         }
     }
-    
-    private fun updateUi(data : String) {
+
+    private fun updateUi(data: String) {
         Logger.d(data)
         val dataSplit = data.split(' ')
         if (dataSplit.isNotEmpty()) {
@@ -65,47 +65,51 @@ class MainActivity : AppCompatActivity() {
             showTankerLevels(firstTankerPercent, secondTankerPercent)
         }
     }
-    
-    private fun showTankerPercents(firstTankerPercent : String, secondTankerPercent : String) {
+
+    private fun showTankerPercents(firstTankerPercent: String, secondTankerPercent: String) {
         binding.firstTankerData.text = firstTankerPercent
         binding.secondTankerData.text = secondTankerPercent
     }
-    
-    private fun showTankerLevels(firstTankerPercent : String, secondTankerPercent : String) {
+
+    private fun showTankerLevels(firstTankerPercent: String, secondTankerPercent: String) {
         var firstTankerLevel = (firstTankerPercent.trimEnd('%').toInt())
         var secondTankerLevel = (secondTankerPercent.trimEnd('%').toInt())
-        
+
         if (firstTankerLevel < 0) firstTankerLevel = 0
         if (secondTankerLevel < 0) secondTankerLevel = 0
-        
+
         if (firstTankerLevel in 0..100) binding.firstTankerIndicator.progress = firstTankerLevel
         if (secondTankerLevel in 0..100) binding.secondTankerIndicator.progress = secondTankerLevel
     }
-    
+
     private val client = OkHttpClient()
-    
-    private fun getDataFromSensorSever(callback : (String) -> Unit) {
+
+    private fun getDataFromSensorSever(callback: (String) -> Unit) {
         val request = Request.Builder().url(url).build()
-        
+
         client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call : Call, e : IOException) {
+            override fun onFailure(call: Call, e: IOException) {
                 Logger.d("failed")
                 Logger.d(e.stackTraceToString())
             }
-            
-            override fun onResponse(call : Call, response : Response) {
+
+            override fun onResponse(call: Call, response: Response) {
                 val body = response.body?.string() ?: ""
                 Logger.d(body)
                 callback(body)
             }
         })
     }
-    
+
     private fun prepareLogger() {
-        Logger.addLogAdapter(AndroidLogAdapter(PrettyFormatStrategy.newBuilder().tag(logTag).build()))
+        Logger.addLogAdapter(
+            AndroidLogAdapter(
+                PrettyFormatStrategy.newBuilder().tag(logTag).build()
+            )
+        )
         Logger.d("Application is Started!")
     }
-    
+
     override fun onDestroy() {
         super.onDestroy()
         mainScope.cancel()
--- esp8266/waterLevelOta.ino
+++ esp8266/waterLevelOta.ino
@@ -10,6 +10,8 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+//sonar lib
+#include <NewPing.h>
 //display consts
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 32
@@ -18,8 +20,8 @@
 #define OLED_RESET -1
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 //wifi station info
-const char* ssid     = "ssid";
-const char* password = "pass";
+const char* ssid     = "-";
+const char* password = "-";
 //static ip info
 IPAddress local_IP(192, 168, 1, 100);
 IPAddress gateway(192, 168, 1, 1);
@@ -28,17 +30,14 @@ IPAddress primaryDNS(8, 8, 8, 8);
 IPAddress secondaryDNS(8, 8, 4, 4);
 //webserver
 ESP8266WebServer server(80);
-//ultra sonic sensors pins
+//ultra sonic sensors pins and data
 const int trigPinT1 = 12;
 const int echoPinT1 = 14;
 const int trigPinT2 = 15;
 const int echoPinT2 = 13;
-//define sound velocity in cm/uS
-#define SOUND_VELOCITY 0.034
-#define CM_TO_INCH 0.393701
-//duration of ultra sonic wave travel
-long duration;
-
+#define MAX_DISTANCE 400
+NewPing firstTanker(trigPinT1, echoPinT1, MAX_DISTANCE);
+NewPing secondTanker(trigPinT2, echoPinT2, MAX_DISTANCE);
 float distanceCmT1;
 int levelT1 = 0;
 float distanceCmT2;
@@ -51,26 +50,23 @@ void setup() {
   delay(10);
 
   startWifi();
-  
+
   setupOta();
 
   startWebServer();
-
-  setupUltraSonicSensors();
 
   setupDisplay();
 }
 
 void loop(void) {
-	  ArduinoOTA.handle();
-  getUltraSonicSensorData(1);
-  getUltraSonicSensorData(2);
+  ArduinoOTA.handle();
+  getUltraSonicSensorData();
   server.handleClient();
   delay(1000);
 }
 
 void setupOta() {
-	  ArduinoOTA.onStart([]() {
+  ArduinoOTA.onStart([]() {
     String type;
     if (ArduinoOTA.getCommand() == U_FLASH) {
       type = "sketch";
@@ -101,7 +97,7 @@ void setupOta() {
       Serial.println("End Failed");
     }
   });
-    ArduinoOTA.begin();
+  ArduinoOTA.begin();
 }
 
 void setupDisplay() {
@@ -121,9 +117,6 @@ void startWifi() {
   }
 
   WiFi.begin(ssid, password);
-  //Serial.print("Connecting to ");
-  //Serial.print(ssid);
-  //Serial.println(" ...");
 
   int i = 0;
   while (WiFi.status() != WL_CONNECTED) {
@@ -132,7 +125,6 @@ void startWifi() {
   }
 
   Serial.println('\n');
-  //Serial.println("Connection established!");
   Serial.print("IP address:\t");
   Serial.println(WiFi.localIP());
 }
@@ -151,51 +143,13 @@ void startWebServer() {
   Serial.println("HTTP server started");
 }
 
-void setupUltraSonicSensors() {
-  pinMode(trigPinT1, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPinT1, INPUT); // Sets the echoPin as an Input
-  pinMode(trigPinT2, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPinT2, INPUT); // Sets the echoPin as an Input
-}
+void getUltraSonicSensorData() {
+  distanceCmT1 = firstTanker.ping_cm();
+  distanceCmT2 = secondTanker.ping_cm();
 
-//if tanker = 1 -> tanker 1 , if tanker = 2 -> tanker 2, duh!
-void getUltraSonicSensorData(int tanker) {
-  int trigPin;
-  int echoPin;
-  float distance = 0;
-  int level = 0;
-  if (tanker == 1) {
-    trigPin = trigPinT1;
-    echoPin = echoPinT1;
-  }
-  else {
-    trigPin = trigPinT2;
-    echoPin = echoPinT2;
-  }
-  // Clears the trigPin
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
-
-  // Calculate the distance
-  distance = duration * SOUND_VELOCITY / 2;
-
-  level = map(distance, 0, 170, 100, 0);
-
-  if (tanker == 1) {
-    levelT1 = level;
-    distanceCmT1 = distance;
-  }
-  else {
-    levelT2 = level;
-    distanceCmT2 = distance;
-  }
+  levelT1 = map(distanceCmT1, 0, 170, 100, 0);
+  levelT2 = map(distanceCmT2, 0, 170, 100, 0);
+  
 
   sprintf(lcdText, "T1 %d%% %d T2 %d%% %d", levelT1, (int) distanceCmT1, levelT2, (int) distanceCmT2);
   writeToDisplay(lcdText);
